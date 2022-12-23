ed8a6fa7d737084695de9f5905db4c122d1c08b5
SixStringsCoder <rshanlon@gmail.com>
2022-12-22 18:33:40 -0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import WordCard from './WordCard.svelte'
+</script>
+
+<svelte:head>
+	<title>Pali Word a Day</title>
+	<meta name="description" content="Pali Word a Day" />
+</svelte:head>
+
+<section>
+	<WordCard />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}	
+</style>
